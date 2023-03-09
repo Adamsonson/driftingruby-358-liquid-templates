09023c4e73783ce3e4885ddbc2ee15dfd4e135b6
adam somora <adamsomora33@gmail.com>
2023-03-09 23:57:25 +0100

    set timer for preview output

--- app/javascript/controllers/previewer_controller.js
+++ app/javascript/controllers/previewer_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="previewer"
 export default class extends Controller {
   static targets = ['input', 'output']
+  static values = { url: String }
 
   connect() {
     this.inputTarget.setAttribute("data-action", "keyup->previewer#update_output")
@@ -10,6 +11,32 @@ export default class extends Controller {
   }
 
   update_output() {
-    this.outputTarget.innerHTML = this.inputTarget.value
+    // this.outputTarget.innerHTML = this.inputTarget.value
+    const params = { content: this.inputTarget.value }
+    const options = {
+      method: "POST",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": this.getMetaValue("csrf-token")
+      },
+      body: JSON.stringify(params)
+    }
+
+    this.currTime = new Date()
+    this.elapsed = this.currTime - this.timer
+    if (this.timer === undefined || this.elapsed > 1000) {
+      fetch(this.urlValue, options)
+        .then(r => r.text())
+        .then(text => this.outputTarget.innerHTML = text)
+      this.timer = this.currTime
+    } else {
+      this.outputTarget.innerHTML = this.inputTarget.value
+    }
+  }
+
+  getMetaValue(name) {
+    const element = document.head.querySelector(`meta[name="${name}"]`)
+    return element.getAttribute("content")
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :mail_templates
   resources :posts
+  resource :previewer, only: :create
   root to: 'welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
